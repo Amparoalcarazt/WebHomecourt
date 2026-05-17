dd830e5ab0c1895723e1cc7e34d3f23f6c9988a1
Monica Guzman <a00838824@tec.mx>
2026-05-16 22:00:24 -0600

    feat: Agregué apartado de collection y se puede navegar a este

---
+++ WebHomecourt/src/components/Collection/CategorySummary.tsx
@@ -0,0 +1,9 @@
+function CollectionSummary() {
+    return (
+        <div>
+            <p>hey</p>
+        </div>
+    )
+}
+
+export default CollectionSummary
---
+++ WebHomecourt/src/pages/Collection.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react';
+import { supabase } from "../lib/supabase";
+import { useNavigate } from 'react-router-dom';
+import { useStoreUser } from '../hooks/useStoreUser.ts'; // Hook for store user, just to get sesh info
+import Nav from '../components/Nav/Nav.tsx';
+import Button from '../components/button.tsx';
+import type { CardSummary } from '../hooks/Collection/collectionTypes.tsx';
+
+// Handle API call
+async function getCollectionSummary(userId: string | null) {
+    const { data, error } = await supabase.rpc("collection_summary", {
+        p_user_id: userId,
+    });
+
+    // Smth died
+    if (error) {
+        console.error("Supabase error:", error.message)
+        throw new Error("Failed to get packs")
+    }
+
+    console.log("raw rpc data:", data);
+    console.log("is array?", Array.isArray(data));
+
+    // Data is not formatted as array, entcs hace un array vacío and sends that will show no user colls
+    if (!Array.isArray(data)) return []
+
+    console.log("raw data:", JSON.stringify(data, null, 2)) // A ver como se ve lo q fue fetched
+
+    return data;
+}
+
+function Collection() {
+    const navigate = useNavigate(); // Switch to diff screen
+    const { storeUser } = useStoreUser(); // Use hook to get basic session and login info
+    const [summary, setSummary] = useState<CardSummary[]>([]); // Will only be one obj but whatever
+
+    // Get user session info 
+    useEffect(() => {
+        if (!storeUser.signedIn) {
+            console.log("User not signed in");
+        } else {
+            console.log(`User ${storeUser.user_id} has ${storeUser.credits} credits`);
+        }
+    }, [storeUser]);
+
+    // Obtain the collection summary 
+    useEffect(() => {
+        async function loadSummary() {
+            try {
+                const result = await getCollectionSummary(storeUser.user_id);
+                setSummary(result);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+
+        loadSummary();
+    }, [storeUser.user_id]);
+
+    return (
+        <div>
+            <Nav current="Store" />
+            <div className="px-4 py-5 md:px-14 md:py-5 bg-Background w-full">
+                {/* Title comp */}
+                <div className="w-full px-3 py-4 md:px-5 md:py-7 bg-violet-950 rounded-2xl shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] outline outline-1 outline-offset-[-1px] outline-black/25 flex flex-col justify-left items-left overflow-hidden">
+                    <h1 className="justify-start text-white title1">Laker Card Store</h1>
+                    <p className="justify-start text-white mt-2 text-xl text-zinc-300">Unlock more cards featuring your favorite players and improve your Dunk Royale game deck</p>
+                </div>
+
+                {/* View store vs colection */}
+                <div className="flex flex-row justify-left mt-10">
+                    <Button
+                        text="STORE"
+                        type="primarydisable"
+                        onClick={() => navigate('/store')}
+                        className="text-2xl font-semibold px-10 mr-8"
+                    />
+
+                    <Button
+                        text="COLLECTION"
+                        type="primary"
+                        onClick={() => { }}
+                        className="text-2xl font-semibold px-10"
+                    />
+                </div>
+
+                {/* Card summary */}
+                
+
+                {/* Card filters */}
+
+                {/* Card collection */}
+            </div>
+        </div>
+    )
+}
+
+export default Collection
