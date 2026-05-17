0952d464282c36e49cafdbdbdaa66c594ba5d24f
Monica Guzman <a00838824@tec.mx>
2026-05-17 17:55:23 -0600

    Trying to install cypress but nothing happens

--- WebHomecourt/cypress.config.ts
+++ WebHomecourt/cypress.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-  },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-  },
+  projectId: "tq75d7",
 
   e2e: {
     setupNodeEvents(on, config) {
