5f701ec8b258861dd849d15590c3c63727f57053
Monica Catalina Guzman Garcia <a00838824@tec.mx>
2026-05-16 23:17:11 -0600

    Feature collection (#258)
    * feat: Agregué apartado de collection y se puede navegar a este
    
    * feat: Summary of card collection progress changes outline color if all cards have been unlocked
    
    * fix: Made outlines thicker for collection summary
    
    * fix: Renamed store and collection headers

---
+++ WebHomecourt/src/hooks/Collection/collectionTypes.tsx
@@ -0,0 +1,10 @@
+export type CardSummary = {
+    unlocked_common: number;
+    total_common: number;
+    unlocked_rare: number;
+    total_rare: number;
+    unlocked_legendary: number;
+    total_legendary: number;
+    unlocked_limited: number;
+    total_limited: number;
+}
