d02597cee6d3f7647a2052b3cf2f078b8ad6c1ab
Monica Guzman <a00838824@tec.mx>
2026-05-18 21:47:51 -0600

    feat: Added basic logic to rotate unlocked cards that are currently just text in a div

--- WebHomecourt/src/components/Collection/CollectionCardRender.tsx
+++ WebHomecourt/src/components/Collection/CollectionCardRender.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { CollectionCard } from '../../hooks/Collection/collectionTypes.tsx';
 import LockedCardFront from './LockedCardFront.tsx';
 
@@ -5,16 +6,28 @@ type CardProp = {
     card: CollectionCard;
 }
 
-function CollectionCardRender ( {card}: CardProp ) {
-    if (!card.user_owned) {
-       return <LockedCardFront card={card} />;
-    } else {
+function CollectionCardRender({ card }: CardProp) {
+    const [cardFront, setCardFront] = useState(true);
+
+    if (card.user_owned) {
         return (
-            <div className="h-96">
-                <h3>{card.player_name}</h3>
-                <p>Unlocked: {card.times_unlocked} times</p>
+            <div onClick={() => setCardFront((prev) => !prev)}>
+                {cardFront ? (
+                    <div>
+                        <h3>{card.player_name}</h3>
+                        <p>Unlocked: {card.times_unlocked} times</p>
+                    </div>
+                ) : (
+                    <div>
+                        <h3>Back stats</h3>
+                    </div>
+                )}
             </div>
         )
+    }
+    // Doesn't own it, will show the back or also useful if smth were to fail 
+    else {
+        return <LockedCardFront card={card} />;
     }
 }
 
